17a70f8ccc0b7d4ebfc13e3a181f6fbef7587909
Khronos Group Webmaster <webmaster@khronos.org>
2017-07-27 21:18:44 -0400

    Remove extraneous H1 headers

--- index.php
+++ index.php
@@ -1,17 +1,8 @@
 <?php
 $static_title = 'Khronos OpenVG Registry';
-$static_breadcrumb = array(
-    '/registry/' => 'Registry',
-    'NOLINK' => 'OpenVG Registry'
-);
+
 include_once("../../assets/static_pages/khr_page_top.php");
 ?>
-
-<h1 class="pagetitle">Khronos OpenVG&trade; Registry</h1>
-<div id="mainformat">
-
-
-<h1 style="text-align:center"> Khronos OpenVG&trade; Registry </h1>
 
 <p> The OpenVG registry contains specifications of the core API;
     specifications of Khronos- and vendor-approved OpenVG extensions;
